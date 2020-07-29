bb2ea867927dcadce84c92c45066c424e0cc7953
Valdeci Rodrigues Junior <valdeci.rjr@hotmail.com>
2020-07-29 19:00:39 -0300

    first

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// adiciona uma classe na tag do modal
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for(let card of cards){
+    card.addEventListener("click", function(){
+        modalOverlay.classList.add('active')
+        
+        const videoId = card.getAttribute("id")
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`;
+    })
+}
+
+// funcionalidade de fechar o modal
+document.querySelector('.close-modal').addEventListener("click", function(){
+    modalOverlay.classList.remove("active");
+    modalOverlay.querySelector("iframe").src = "";
+})
